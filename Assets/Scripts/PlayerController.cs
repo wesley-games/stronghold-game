d4c9141800d731c8bdcb7c37cf9ea504ae002edb
wesleyallanrocha <wesleyallanrocha@gmail.com>
2018-08-31 01:36:42 -0300

    Acrescentando alguns materiais;

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+    }
+}
